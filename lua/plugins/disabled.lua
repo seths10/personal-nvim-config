c65bcabfa0fde35dcf2c640538a2d76d8794902f
Seth Addo <addoseth35@gmail.com>
2024-05-22 15:31:23 +0000

    initial commit

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- {  "rcarriga/nvim-notify", enabled = true }, -- Annoying af
+  -- { "akinsho/bufferline.nvim", enabled = true }, -- Not necessary (Harpoon!)
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = true }
+}
